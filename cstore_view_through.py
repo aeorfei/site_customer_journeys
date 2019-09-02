b4423441bbb1e11a56dc51e941bf7f0c332807ca
Alessandro Orfei <orfei@upside-services.com>
2019-09-02 10:23:18 -0400

    changed import and some bug fixes

--- cstore_view_through.py
+++ cstore_view_through.py
@@ -1,11 +1,15 @@
 
-import generate_site_customer_journeys as gs
+from generate_site_customer_journeys import get_crawledtxuuid_by_usertxuuid
+from generate_site_customer_journeys import get_user_card_ids
+from generate_site_customer_journeys import get_list_if_string
+from generate_site_customer_journeys import get_incremental
+from generate_site_customer_journeys import get_crawled_txns
 from datetime import timedelta
 import pandas as pd
 from tqdm import *
 
 def get_user_unique_f6l4_cards(user_uuid):
-    user_card_ids = gs.get_user_card_ids(user_uuid)
+    user_card_ids = get_user_card_ids(user_uuid)
     unique_fsl4 = list(set([x.split('-')[1] + '-' + x.split('-')[2] for x in user_card_ids]))
     return unique_fsl4
 
@@ -15,37 +19,37 @@ def get_usertxuuid_viewthrough(site_uuid,
                                    user_uuid,
                                    usertx_uuid,
                                    site_crawledtx_uuids,
-                                   view_through_source_terminal,
+                                   viewthrough_source_terminal,
                                    timewindow_minutes,
                                    site_crawled_txns):
 
-    view_through_source_terminal = gs.get_list_if_string(view_through_source_terminal)
-    crawledtx_uuid = gs.get_crawledtxuuid_by_usertxuuid(usertx_uuid, site_uuid)
-    usertx_uuid_timestamp = site_crawled_txns.query("transactionUuid == @crawledtx_uuid")['transactionTimestamp']
+    viewthrough_source_terminal = get_list_if_string(viewthrough_source_terminal)
+    crawledtx_uuid = get_crawledtxuuid_by_usertxuuid(usertx_uuid, site_uuid)
+    usertx_uuid_timestamp = site_crawled_txns.query("transactionUuid == @crawledtx_uuid")['transactionTimestamp'].reset_index(drop=True)[0]
 
     start_window = usertx_uuid_timestamp - timedelta(minutes=timewindow_minutes)
     end_window = usertx_uuid_timestamp + timedelta(minutes=timewindow_minutes)
 
     user_f6l4 = get_user_unique_f6l4_cards(user_uuid)
 
-    viewthrough_result = site_crawled_txns.query("F6L4 in @user_f6l4" \
-                                                 "and sourceTerminal in @view_through_source_terminal" \
-                                                 "and transactionTimestamp >= @start_window" \
-                                                 "and transactionTimestamp <= @end_window" \
-                                                 "and transactionUuid not in @site_crawledtx_uuid")
+    viewthrough_result = site_crawled_txns.query("F6L4 in @user_f6l4 \
+                                                  and sourceTerminal in @viewthrough_source_terminal \
+                                                  and transactionTimestamp >= @start_window \
+                                                  and transactionTimestamp <= @end_window \
+                                                  and transactionUuid not in @site_crawledtx_uuids")
 
     return viewthrough_result
 
-def get_view_through_for_site(site_uuid):
-    site_incremental = gs.get_incremental(site_uuid,
+def get_viewthrough_for_site(site_uuid):
+    site_incremental = get_incremental(site_uuid,
                                           source_terminal_filter='OUTSIDE',
                                           user_uuid_filter=None,
                                           add_crawledtx_uuid=True)\
                          .reset_index(drop=True)
 
-    site_crawledtx_uuids = site_incremental.crawledTxUuid.unique()
+    site_crawledtx_uuids = list(site_incremental.crawledTxUuid.unique())
 
-    site_crawled_txns = gs.get_crawled_txns(site_uuid=site_uuid) \
+    site_crawled_txns = get_crawled_txns(site_uuid=site_uuid) \
                         .assign(transactionTimestamp=lambda x: pd.to_datetime(x['transactionTimestamp']))
 
     viewthrough_df =pd.DataFrame()
@@ -54,7 +58,7 @@ def get_view_through_for_site(site_uuid):
                                                             user_uuid=row['useruuid'],
                                                             usertx_uuid=row['id'],
                                                             site_crawledtx_uuids=site_crawledtx_uuids,
-                                                            view_through_source_terminal='INSIDE',
+                                                            viewthrough_source_terminal='INSIDE',
                                                             timewindow_minutes=30,
                                                             site_crawled_txns=site_crawled_txns)
 
@@ -69,6 +73,7 @@ def get_view_through_for_site(site_uuid):
         viewthrough_df = pd.concat([viewthrough_df, usertxuuid_viewthrough]).reset_index(drop=True)
 
     return viewthrough_df
+
 
 
 
